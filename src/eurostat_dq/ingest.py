7423f22266ce857b7ec98d89ffc6ac7f96636e7e
Ricsi <g4jd80@inf.elte.hu>
2026-07-20 18:39:56 +0200

    figured out how to retrieve all the data from JSON-stat format

--- src/eurostat_dq/ingest.py
+++ src/eurostat_dq/ingest.py
@@ -2,7 +2,7 @@ import eurostat
 import pandas as pd
 import requests, json
 from pathlib import Path
-from .config import PROJECT_ROOT, BASE_REQUEST_URL
+from .config import PROJECT_ROOT, BASE_REQUEST_URL, DATASETS
 
 def fetch_dataset(code: str, *, use_cache: bool = True) -> pd.DataFrame:
     """Fetches dataset with eurostat.get_data_df and if use_cache=True it searches the data/raw folder first before fetching from the web"""
@@ -23,10 +23,13 @@ def fetch_dataset_json(code: str, *, use_cache: bool = True, **filters) -> pd.Da
 
     Filters must be using the EurostatAPI's request structure, but in a dictionary e.g.: {"age":"TOTAL", ...., "geo":"HU11"}
     (format and lang are given)"""
-    if filters:
-        filters_label = f"_{'_'.join(k for k, v in filters.items())}"
-    else:
-        filters_label = ""
+
+    # Update "filters" with mandatory filters
+    if code in DATASETS.keys():
+        filters.update(DATASETS[code].filters)
+    filters.update({"format":"JSON", "lang":"en"})
+    
+    filters_label = f"_{'_'.join(sorted(filters.values()))}"
     
     cache_path = PROJECT_ROOT / "data" / "raw" / f"{code}{filters_label}.json"
     if (use_cache and cache_path.exists()):
@@ -34,7 +37,7 @@ def fetch_dataset_json(code: str, *, use_cache: bool = True, **filters) -> pd.Da
         return pd.DataFrame(decode_json(cache_path))
     else:
         resp = requests.get(f"{BASE_REQUEST_URL}/{code}",
-            params= filters.update({"format":"JSON", "lang":"en"}),
+            params=filters,
             timeout=30)
         resp.raise_for_status()
         d = resp.json()
